760173de11d43b5d2cd9d37a3cecbc6cf33f2f32
Hiroyuki Yoshihara <hiroyuki.yoshihara5211@gmail.com>
2018-04-13 17:08:32 +0900

    Add files via upload

---
+++ cansat2018.ino
@@ -0,0 +1,22 @@
+/**
+ @file cansat2018.ino
+ @brief CanSat Project 2018
+ @author Hiroyuki Yoshihara
+ @date Created: 20180413
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "Arduino.h"
+#include <HardwareSerial.h>
+#include "constant.h" //全ての定数はここ
+#include "cansat.h"
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
