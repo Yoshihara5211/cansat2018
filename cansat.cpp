866462b6774cd749167ecbdad74dcc073e7afa2c
1014ksg <38559086+1014ksg@users.noreply.github.com>
2018-05-29 08:56:52 +0900

    ++ guidance2

--- cansat.cpp
+++ cansat.cpp
@@ -49,3 +49,49 @@ void Cansat::guidance1(float nowLat, float nowLon, float nowDeg, float goalLat, 
     leftMotor.go(170 * (1 - _bodyAngle / 180));
   }
 }
+
+void Cansat::guidance2(float nowLat, float nowLon, float goalLat, float goalLon) {
+  // Lon=経度=x
+  // Lat=緯度=y
+  ////自分のGPSの値　初期値
+  float Lon1=nowLon;
+  float Lat1=nowLat;
+  ////モータの駆動
+  rightMotor.go(255);
+  leftMotor.go(255);
+  delay(10000);
+  ////動いた後のGPSの値
+  float Lon2=gps.lon
+  float Lat2=gps.lat;
+  float xvel2g=(goalLon-Lon2)/sqrt(pow(goalLon-Lon2, 2) + pow(goalLat-Lat2, 2));
+  float yvel2g=(goalLat-Lat2)/sqrt(pow(goalLon-Lon2, 2) + pow(goalLat-Lat2, 2));
+  float deg2g=atan(yvel2g/xvel2g);
+  float xvel12=(Lon2-Lon1)sqrt(pow(goalLon-Lon2, 2) + pow(goalLat-Lat2, 2));
+  float yvel12=(Lat2-Lat1)sqrt(pow(goalLon-Lon2, 2) + pow(goalLat-Lat2, 2)); 
+  float deg12=atan(yvel12/xvel12);
+
+////モータの駆動
+  if (deg12>deg2g) {//左を上げる
+    rightMotor.go(255*0.8);
+    leftMotor.go(255);
+  } else if (deg2g>deg12) { //右を上げる
+    rightMotor.go(255);
+    leftMotor.go(255*0.8);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
