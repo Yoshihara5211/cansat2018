fb4e54e780603583710529960c0eb8ba1b3b70c5
Hiroyuki Yoshihara <hiroyuki.yoshihara5211@gmail.com>
2018-07-17 16:10:38 +0900

    update

--- cansat.cpp
+++ cansat.cpp
@@ -85,12 +85,12 @@ void Cansat::sensor() {
   if (state != FLYING) sendXbee();
   Serial.println("radio is ok");
 
-  if (gps.lon < 1 && gps.lat < 1) {
-    leftMotor.stop();
-    rightMotor.stop();
-  } else {
-    guidance1(gps.lon, gps.lat, compass.deg, destLon, destLat);
-  }
+//  if (gps.lon < 1 && gps.lat < 1) {
+//    leftMotor.stop();
+//    rightMotor.stop();
+//  } else {
+//    guidance1(gps.lon, gps.lat, compass.deg, destLon, destLat);
+//  }
 }
 
 void Cansat::writeSd() {
